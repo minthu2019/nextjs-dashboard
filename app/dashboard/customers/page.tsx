43fcedbde6c6cd9c7e381b655557cce476dd234d
Min Thu Khaing <mtk.min2003@gmail.com>
2024-01-07 13:15:26 +0630

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const customers = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  )
+}
+
+export default customers
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Dashboard Page
+      <div>Hello My Name Is  Min tHu khaing</div>
+    </div>
+  )
+}
+
+export default page
